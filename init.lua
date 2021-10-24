d14c6fa1031a8fd559da2fa1f2b9db84dc58c373
Jonathan Bowman <git@bowmanjd.com>
2021-10-24 05:13:09 -0400

    feat: enabled treesitter

--- init.lua
+++ init.lua
@@ -56,6 +56,36 @@ require 'paq' {
 
 cmd 'colorscheme defnoche'
 
+local ts_status, ts = pcall(require, 'nvim-treesitter.configs')
+if(ts_status) then
+  ts.setup {
+    ensure_installed = {
+      'bash',
+      'c',
+      'css',
+      'dart',
+      'go',
+      'html',
+      'java',
+      'javascript',
+      'jsdoc',
+      'json',
+      'kotlin',
+      'lua',
+      'python',
+      'rust',
+      'toml',
+      'vim',
+      'yaml'
+    },
+    highlight = {
+      enable = true,              -- false will disable the whole extension
+      -- disable = { "c", "rust" },
+      additional_vim_regex_highlighting = false,
+    },
+  }
+end
+
 local ll_status, lualine = pcall(require, "lualine")
 if(ll_status) then
   lualine.setup()
